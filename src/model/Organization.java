4f0e756b8cd0c201eefb9062a5b0743c002171de
vfaddey <vfaddey@yandex.ru>
2024-02-11 15:01:08 +0300

    try

--- src/model/Organization.java
+++ src/model/Organization.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 public class Organization implements Comparable<Organization> {
 
-    private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+    private final Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
     private java.time.LocalDate creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
