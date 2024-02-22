a648aa7f6e4383ed66b75a9fb1ab0f32ba05c1bc
vfaddey <vfaddey@yandex.ru>
2024-02-22 10:17:12 +0300

    стек вызовов скриптов

--- src/commands/ExecuteFile.java
+++ src/commands/ExecuteFile.java
@@ -5,8 +5,10 @@ import interfaces.CommandWithParameters;
 import managers.ConsoleHandler;
 
 import java.io.IOException;
+import java.util.Stack;
 
 public class ExecuteFile extends Command implements CommandWithParameters {
+    private Stack<String> filenamesStack = new Stack<>();
     public ExecuteFile(String nameInConsole) {
         super(nameInConsole, "<Путь к файлу> Выполняет содержимое скрипта", "Код скрипта выполнен!");
     }
@@ -14,8 +16,14 @@ public class ExecuteFile extends Command implements CommandWithParameters {
     @Override
     public void execute(String... parameters) throws WrongParameterException {
         try {
-            ConsoleHandler.ScriptHandler.readCommands(parameters[0], collectionManager.getConsoleHandler().getCommandManager());
-            printSuccess();
+            if (!filenamesStack.contains(parameters[0])) {
+                ConsoleHandler.ScriptHandler.readCommands(parameters[0], collectionManager.getConsoleHandler().getCommandManager());
+                filenamesStack.push(parameters[0]);
+                printSuccess();
+            } else {
+                throw new RecursionExecutionException("Файл " + parameters[0] + "уже был вызван.");
+            }
+
         } catch (IOException | WrongParameterException | IncorrectFilenameException | ElementNotFoundException | CommandNotExistsException | NullUserRequestException e) {
             throw new WrongParameterException("Файл не найден или нет доступа к файлу.");
         } catch (RecursionExecutionException e) {
