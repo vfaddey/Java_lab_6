bf54df2014e282d40a09ca72ed0639b7c4717ce9
vfaddey <vfaddey@yandex.ru>
2024-02-26 15:37:15 +0300

    help change

--- src/commands/Help.java
+++ src/commands/Help.java
@@ -11,7 +11,7 @@ public class Help extends Command implements CommandWithoutParameters {
 
     @Override
     public void execute() {
-        int padding = 40;
+        int padding = 35;
         HashSet<Command> commands = collectionManager.getSender().getConsoleHandler().getCommandManager().getCommands();
         StringBuilder output = new StringBuilder("Все доступные команды:\n");
         for (Command command : commands) {
