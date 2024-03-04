6b4e4dadb6669d307d74f5c82b9e9b1b7378b8cb
vfaddey <vfaddey@yandex.ru>
2024-03-04 14:18:01 +0300

    organizationtype toString overriden

--- src/model/OrganizationType.java
+++ src/model/OrganizationType.java
@@ -1,9 +1,21 @@
 package model;
 
 public enum OrganizationType {
-    COMMERCIAL,
-    PUBLIC,
-    GOVERNMENT,
-    PRIVATE_LIMITED_COMPANY,
-    OPEN_JOINT_STOCK_COMPANY
+    COMMERCIAL("Коммерческая"),
+    PUBLIC("Публичная"),
+    GOVERNMENT("Государственная"),
+    PRIVATE_LIMITED_COMPANY("ООО"),
+    OPEN_JOINT_STOCK_COMPANY("ОАО");
+
+    private final String translation;
+
+    OrganizationType(String translation) {
+        this.translation = translation;
+    }
+
+    @Override
+    public String toString() {
+        return "OrganizationType{"
+                + translation + '}';
+    }
 }
