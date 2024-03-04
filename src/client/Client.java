d5bac2265ef3e04e8c244f670f6ff81b9345b966
vfaddey <vfaddey@yandex.ru>
2024-03-04 13:46:29 +0300

    client-server ship

---
+++ src/client/Client.java
@@ -0,0 +1,26 @@
+package client;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 8888;
+
+    public static void main(String[] args) {
+        try (Socket serverSocket = new Socket(SERVER_ADDRESS, SERVER_PORT)) {
+            OutputStream output = serverSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            writer.println("hello, server");
+
+            InputStream input = serverSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String response = reader.readLine();
+            System.out.println("Ответ сервера: " + response);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,37 @@
 package server;
 
+import javax.net.ssl.SSLServerSocket;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    private static final int PORT = 8888;
+
+    public static void main(String[] args) {
+        String response;
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Сервер запущен! Порт: " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Подключился клиент: " + serverSocket);
+
+                InputStream input = clientSocket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                String request = reader.readLine();
+                if (request.equals("hello, server")) {
+                    response = "hello, client";
+                } else {
+                    response = "(";
+                }
+                OutputStream output = clientSocket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                writer.println(response);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
